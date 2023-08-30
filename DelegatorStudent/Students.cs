fbcd3efdb6c4a81aa94ea18621dec1533bc1e3a5
Jihye Yang <s2127@e-mirim.hs.kr>
2023-08-30 12:06:50 +0900

    딜리게이터와 람다를 이용해서 다양한 출력 구현!

--- DelegatorStudent/Students.cs
+++ DelegatorStudent/Students.cs
@@ -8,5 +8,24 @@ namespace DelegatorStudent
 {
     internal class Students
     {
+        private List<Student> listOfstudent = new List<Student>();
+        public delegate void PrintProcess(Student s);
+
+        public void Add(Student s)
+        {
+            listOfstudent.Add(s);
+        }
+
+        public void Print(PrintProcess process)
+        {
+            foreach (Student s in listOfstudent)
+            {
+                process(s);
+            }
+        }
+        public void Print() 
+        {
+            Print((s) => Console.WriteLine(s));
+        }
     }
 }
